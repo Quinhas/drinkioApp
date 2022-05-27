2bc0cb33634ba565dcf7759ffb23ef6c85da7320
Quinhas <ln.santana08@gmail.com>
2022-05-27 13:59:01 -0300

    refactor: add a feedback message when doesn't have a favorite category or drink

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,5 +1,6 @@
+import { FontAwesome5 } from "@expo/vector-icons";
 import _ from "lodash";
-import { Flex, HStack, ScrollView, Text, VStack } from "native-base";
+import { Flex, HStack, Icon, ScrollView, Text, VStack } from "native-base";
 import React from "react";
 import useFavorites from "../../hooks/useFavorites";
 import { CategoryCard } from "./components/CategoryCard";
@@ -34,22 +35,46 @@ export function Favorites() {
           Categories
         </Text>
 
-        <ScrollView
-          horizontal
-          persistentScrollbar={false}
-          showsHorizontalScrollIndicator={false}
-        >
-          <HStack space={4}>
-            {favoritesCategories.map((id) => (
-              <CategoryCard
-                key={id}
-                id={id}
-                isFirst={_.first(favoritesCategories) === id}
-                isLast={_.last(favoritesCategories) === id}
-              />
-            ))}
-          </HStack>
-        </ScrollView>
+        {favoritesCategories.length > 0 ? (
+          <ScrollView
+            horizontal
+            persistentScrollbar={false}
+            showsHorizontalScrollIndicator={false}
+          >
+            <HStack space={4}>
+              {favoritesCategories.map((id) => (
+                <CategoryCard
+                  key={id}
+                  id={id}
+                  isFirst={_.first(favoritesCategories) === id}
+                  isLast={_.last(favoritesCategories) === id}
+                />
+              ))}
+            </HStack>
+          </ScrollView>
+        ) : (
+          <Flex
+            justify={"center"}
+            align={"center"}
+            mx={"1.5rem"}
+            direction={"row"}
+          >
+            <Icon
+              as={FontAwesome5}
+              name={"sad-tear"}
+              size={"2.25rem"}
+              color={"muted.500"}
+            />
+            <Text
+              fontWeight={"medium"}
+              color={"muted.500"}
+              fontSize={"1.25rem"}
+              ml={"1rem"}
+            >
+              You don't have any favorite categories.
+            </Text>
+          </Flex>
+        )}
       </Flex>
 
       <Flex m={"1.5rem"}>
@@ -57,11 +82,30 @@ export function Favorites() {
           Drinks
         </Text>
 
-        <VStack space={4}>
-          {favoritesDrinks.map((id) => (
-            <DrinkCard key={id} id={id} />
-          ))}
-        </VStack>
+        {favoritesDrinks.length > 0 ? (
+          <VStack space={4}>
+            {favoritesDrinks.map((id) => (
+              <DrinkCard key={id} id={id} />
+            ))}
+          </VStack>
+        ) : (
+          <Flex justify={"center"} align={"center"} direction={"row"}>
+            <Icon
+              as={FontAwesome5}
+              name={"sad-tear"}
+              size={"2.25rem"}
+              color={"muted.500"}
+            />
+            <Text
+              fontWeight={"medium"}
+              color={"muted.500"}
+              fontSize={"1.25rem"}
+              ml={"1rem"}
+            >
+              You don't have any favorite drinks.
+            </Text>
+          </Flex>
+        )}
       </Flex>
     </>
   );
